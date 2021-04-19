0b84fd9667f4ca1f2ccbbee630f29a6de3e084bd
Tentrilix <37255556+Tentrilix@users.noreply.github.com>
2021-04-19 13:06:41 +0200

    logging works. heck yeah

--- src/JavaReImplementations/Random.java
+++ src/JavaReImplementations/Random.java
@@ -5,7 +5,7 @@ public class Random {
 
 	public int NextRandom(int min, int max) {
 		java.util.Random r = new java.util.Random();
-		int delta = max - min;
+		int delta = (max + 1) - min; //beacuse java random goes from 1 to value
 		return r.nextInt(delta) + min;
 	}
 }
--- src/Map.java
+++ src/Map.java
@@ -2,6 +2,10 @@ import Enums.TileTypes;
 import JavaReImplementations.Random;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Arrays.*;
+import java.util.Iterator;
 import java.util.logging.Level;
 
 
@@ -10,6 +14,8 @@ public class Map {
 	private int HEIGHT;
 
 	private MapTile[][] tiles;
+
+	Random r = new Random();
 
 	public Map(int WIDTH, int HEIGHT) {
 		this.WIDTH = WIDTH;
@@ -36,7 +42,6 @@ public class Map {
 
 	private void GenerateLandMass() {
 		Logging.Log("Generating land", "Setup.log" , getClass().getName(), Level.INFO);
-		Random r = new Random();
 		int DELTA = 3;
 		int linePos = (int) Math.floor((double) WIDTH / 2) - 10;
 		Point previous = new Point(r.NextRandom(linePos - DELTA, linePos + DELTA), 0);
@@ -53,6 +58,49 @@ public class Map {
 
 			previous = current;
 		}
+	}
+
+	private void GenerateLakes() {
+		int noLakes = r.NextRandom(2, 5);
+		int[] sizeofLakes = new int[noLakes];
+		Point curr;
+		ArrayList<Point> neighbours;
+
+		for (int i = 0; i < noLakes; i++) {
+			sizeofLakes[i] = r.NextRandom(4, 9);
+		}
+
+		for (int i = 0; i < noLakes; i++) {
+			curr = getNewLakeCoords();
+
+			for (int j = 0; j < sizeofLakes[i]; j++) {
+				neighbours = getNeighbours(curr);
+			}
+		}
+	}
+
+	private Point getNewLakeCoords() {
+		Point curr = new Point(0, 0);
+
+		while (tiles[curr.x][curr.y].getType()  != TileTypes.GRASS) {
+			curr.x = r.NextRandom(0, HEIGHT - 1);
+			curr.y = r.NextRandom(0, WIDTH - 1);
+		}
+
+		return curr;
+	}
+
+	private java.util.ArrayList<Point> getNeighbours(Point curr) {
+		java.util.ArrayList<Point> nbs = new java.util.ArrayList<>();
+		for (int i = curr.y - 1; i <= curr.y + 1; i++) {
+			for (int j = curr.x - 1; j <= curr.x + 1; j++) {
+				nbs.add(new Point(i, j));
+			}
+		}
+
+		nbs.removeIf(current -> tiles[current.y][current.x].getType() == TileTypes.LAKE);
+
+		return nbs;
 	}
 
 	@Override
